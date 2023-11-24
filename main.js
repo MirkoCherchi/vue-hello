ca5c914a2b71c15212adc9e6461dedd17a1fc0ba
MirkoCherchi <mirkocherchi1992@gmail.com>
2023-11-24 15:24:41 +0100

    feat: bonus

--- main.js
+++ main.js
@@ -4,7 +4,8 @@ const { createApp } = Vue
     data() {
       return {
         titolo: 'Benvenuto in Vue Js',
-        color: 'coloretitolo'
+        color: 'coloretitolo',
+        immagine: 'https://cdn.sanity.io/images/8edntncj/production/7cb22a8a9885468d96662f6ece61ceb2bc95666d-500x300.png'
       }
     }
   }).mount('#app')
